69ff669b39db5573ea93b5c54ebc4a9f3492ac6d
Miku NPC <miku.npc@gmail.com>
2023-09-04 14:30:03 -0400

    add pixels size node

--- imagetext.py
+++ imagetext.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image, ImageFont, ImageDraw
 import os
 
-class TextImage:
+class TextImagePoint:
     def __init__(self):
         pass
 
@@ -12,19 +12,19 @@ class TextImage:
         return {
             "required": {
                 "text": ('STRING', { 'multiline': True, 'default': '' }),
-                "size": ("Integer", {"default": 20, "min": 1, "max": 1000}),
-                "width": ("Integer", {"default": 800, "min": 64, "max": 10000}),
-                "height": ("Integer", {"default": 1200, "min": 64, "max": 10000}),
-                "x_pos": ("Integer", {"default": 400, "min": 0, "max": 10000}),
-                "y_pos": ("Integer", {"default": 400, "min": 0, "max": 10000}),
-                "font": (os.listdir(r'C:\Windows\fonts'),),
+                "size": ("INT", {"default": 20, "min": 1, "max": 1000}),
+                "width": ("INT", {"default": 800, "min": 64, "max": 10000}),
+                "height": ("INT", {"default": 1200, "min": 64, "max": 10000}),
+                "x_pos": ("INT", {"default": 400, "min": 0, "max": 10000}),
+                "y_pos": ("INT", {"default": 400, "min": 0, "max": 10000}),
+                "font": ([font for font in os.listdir(r'C:\Windows\fonts') if font.split(".")[-1] == "ttf"],),
             }
         }
 
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "generate_text"
     OUTPUT_NODE = True
-    CATEGORY = "Image/Text Image"
+    CATEGORY = "image"
 
     def generate_text(self, text, size, width, height, x_pos, y_pos, font):
         # generate image with PIL
@@ -40,7 +40,45 @@ class TextImage:
 
         return (torch.cat([out_image], 0),)
 
+class TextImagePixel:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "text": ('STRING', { 'multiline': True, 'default': '' }),
+                "size": ("INT", {"default": 20, "min": 1, "max": 1000}),
+                "width": ("INT", {"default": 800, "min": 64, "max": 10000}),
+                "height": ("INT", {"default": 1200, "min": 64, "max": 10000}),
+                "x_pos": ("INT", {"default": 400, "min": 0, "max": 10000}),
+                "y_pos": ("INT", {"default": 400, "min": 0, "max": 10000}),
+                "font": ([font for font in os.listdir(r'C:\Windows\fonts') if font.split(".")[-1] == "ttf"],),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "generate_text"
+    OUTPUT_NODE = True
+    CATEGORY = "image"
+
+    def generate_text(self, text, size, width, height, x_pos, y_pos, font):
+        # generate image with PIL
+        image = Image.new('RGB', (width, height))
+        
+        size = size * 0.75
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.truetype("arial.ttf", size)
+        draw.text((x_pos, y_pos), text, font=font)
+
+        # convert to tensor
+        out_image = np.array(image.convert("RGB")).astype(np.float32) / 255.0
+        out_image = torch.from_numpy(out_image).unsqueeze(0)
+
+        return (torch.cat([out_image], 0),)
         
 NODE_CLASS_MAPPINGS = {
-    "Text Image": TextImage,
+    "Generate Text Image (Point)": TextImagePoint,
+    "Generate Text Image (Pixels)": TextImagePixel,
 }
